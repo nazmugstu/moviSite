005d24d3f8a72e64a780c9bde0c6aea744ed1b39
alam0164088 <168212623+alam0164088@users.noreply.github.com>
2025-04-29 21:49:08 +0600

    Fix: Add Render domain to ALLOWED_HOSTS

--- MovieSite/settings.py
+++ MovieSite/settings.py
@@ -5,7 +5,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'your-secret-key-here'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'movi-web.onrender.com']
+
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
